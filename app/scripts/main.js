d06d00916e3420b5cb95a2d8555e1a88dd278cc3
David Guijarro <guijarro.dav@gmail.com>
2015-05-06 16:45:34 +0200

    Slide animation redone

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,8 @@
 /* jshint devel:true */
-
-$(".designer").click(function() {
-	$(this).children().transition('fly down');
-	if ($(this).parent().hasClass("three column")) {
-		$(this).parent().removeClass("three column").addClass("one column");		
-	} else if ($(this).parent().hasClass("one column")) {
-		$(this).parent().removeClass("one column").addClass("three column");
-	}
-});
+(function() {
+	$("section").click(function() {
+		$(this).toggleClass('full');
+		$(this).siblings().toggle();
+		$(this).children().transition('fly down');
+	});
+})();
